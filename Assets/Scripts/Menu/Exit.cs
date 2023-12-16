f3e6fb6ccecdaa01455df7ad361a584a82830ac1
guskirkpatrick <kirkpagr@miamioh.edu>
2023-12-15 22:05:10 -0500

    complete :)

--- Assets/Scripts/Menu/Exit.cs
+++ Assets/Scripts/Menu/Exit.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 //using static System.Net.Mime.MediaTypeNames;
+//using static System.Net.Mime.MediaTypeNames;
 
 public class ExitGame : MonoBehaviour
 {
     // This method will be called when the button is clicked
     public void Exit()
     {
-        // This line quits the application (works in standalone builds)
-        // Note: This won't work in the Unity Editor, only in a built application
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
